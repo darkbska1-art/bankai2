8576f87dbaf34cb2e7336a1bb58eb6277cb9f26f
Wiw <darkbska1@gmail.com>
2026-09-03 14:06:50 +0300

    yapılan değişiklik

--- commands/yardı.js
+++ commands/yardı.js
@@ -114,7 +114,7 @@ module.exports = {
                     ["boostkanal", "boost kanalını ayarlar."],
                     ["boostkapat", "boost mesajını devre dışı bırakır."],
                     ["seviyesistem", "seviye sistemini ayarlar."],
-                    ["modlog", "log kanalını ayarlar."],
+                    
                 ]
             }
         };
